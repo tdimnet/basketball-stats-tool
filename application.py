3ca9b80c65cd9e4167c48c9551b004a293b1eefd
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-03-27 15:01:14 +0100

    Add list expression

--- application.py
+++ application.py
@@ -2,12 +2,14 @@ from constants import PLAYERS, TEAMS
 
 
 def main():
-    print("===")
-    print(PLAYERS)
-    print("===")
-    print("===")
-    print(TEAMS)
-    print("===")
+    cleaned_players_data = [{
+        'name': player.get('name'),
+        'guardians': player.get('guardians'),
+        'experience': True if player.get('experience') == 'YES' else False,
+        'height': int(player.get('height')[:2])
+    } for player in PLAYERS]
+
+    print(cleaned_players_data)
 
 
 if __name__ == "__main__":
