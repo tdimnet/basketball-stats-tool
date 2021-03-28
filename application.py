0251bf0345fc6560fcabb7050e4e05afe2f2ee0a
Thomas Dimnet <thomas.dimnet@gmail.com>
2021-03-28 15:34:24 +0200

    Working on balance_teams fonction

--- application.py
+++ application.py
@@ -1,17 +1,6 @@
 from constants import PLAYERS, TEAMS
 
 
-def main():
-    cleaned_players_data = [{
-        'name': player.get('name'),
-        'guardians': player.get('guardians'),
-        'experience': True if player.get('experience') == 'YES' else False,
-        'height': int(player.get('height')[:2])
-    } for player in PLAYERS]
-
-    print(cleaned_players_data)
-
-
 def print_menu():
     print("""
     BASKETBALL TEAM STATS TOOL
@@ -32,5 +21,35 @@ def print_menu():
     return user_choice
 
 
+def balance_teams(players):
+    number_of_players_per_team = int(len(players) / len(TEAMS))
+    
+    panthers_team = players[0:6]
+    bandits_team = players[6:12]
+    warriors_team = players[12:18]
+
+    print("+++++")
+    print(panthers_team)
+    print("+++++")
+    print("+++++")
+    print(bandits_team)
+    print("+++++")
+    print("+++++")
+    print(warriors_team)
+    print("+++++")
+    
+
+
+def main():
+    cleaned_players_data = [{
+        'name': player.get('name'),
+        'guardians': player.get('guardians'),
+        'experience': True if player.get('experience') == 'YES' else False,
+        'height': int(player.get('height')[:2])
+    } for player in PLAYERS]
+
+    balance_teams(cleaned_players_data)
+
+
 if __name__ == "__main__":
-    print_menu()
+    main()
